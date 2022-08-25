6ffc688e68964a70168ec40af0d94167f0eb14af
deependra.karki <deependra.karki@chuchuro.com>
2022-08-25 10:26:10 +0545

    button package

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import buttonVue from './button.vue'
+
+export default {
+ install(Vue, options) {
+  // Let's register our component globally
+  // https://vuejs.org/v2/guide/components-registration.html
+  Vue.component("buttonVue", button);
+ }
+};
